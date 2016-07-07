868de3ac12fe5bf0c3402816f966bc22b7e251d4
Markus Huggler <markus.huggler@ecologic.ch>
2016-07-07 10:28:38 +0200

    Upgrade to current pdfMake.js (and directory structure)

--- src/browser-extensions/pdfMake.js
+++ src/browser-extensions/pdfMake.js
@@ -4,8 +4,9 @@
 'use strict';
 
 var vfsDefault = require('../vfs-fonts/roboto.js');
-var PdfPrinter = require('pdfmake');
-var saveAs = require('../libs/fileSaver');
+var PdfPrinter = require('../printer');
+var FileSaver = require('../../libs/FileSaver.js/FileSaver');
+var saveAs = FileSaver.saveAs;
 var vfs = require('./virtual-fs');
 
 var defaultClientFonts = {
@@ -20,7 +21,7 @@ var defaultClientFonts = {
 function Document(docDefinition, fonts, vfs) {
 	this.docDefinition = docDefinition;
 	this.fonts = fonts || defaultClientFonts;
-	this.vfs = vfs || vfsDefault;
+	this.vfs = vfs;
 }
 
 Document.prototype._createDoc = function(options, callback) {
@@ -49,14 +50,31 @@ Document.prototype._getPages = function(options, cb){
 };
 
 Document.prototype.open = function(message) {
-	// we have to open the window immediately and store the reference
+		// we have to open the window immediately and store the reference
 	// otherwise popup blockers will stop us
 	var win = window.open('', '_blank');
-
+	
 	try {
-		this.getDataUrl(function(result) {
-			win.location.href = result;
-		});
+		this.getBuffer(function (result) {
+			var blob;
+			try {
+				blob = new Blob([result], { type: 'application/pdf' });
+			} catch (e) {
+				// Old browser which can't handle it without making it an byte array (ie10) 
+				if (e.name == "InvalidStateError") {
+					var byteArray = new Uint8Array(result);
+					blob = new Blob([byteArray.buffer], { type: 'application/pdf' });
+				}
+			}
+			
+			if (blob) {
+				var urlCreator = window.URL || window.webkitURL;
+				var pdfUrl = urlCreator.createObjectURL( blob );
+				win.location.href = pdfUrl;
+			} else {
+				throw 'Could not generate blob';
+			}
+		},  { autoPrint: false });
 	} catch(e) {
 		win.close();
 		throw e;
@@ -65,21 +83,35 @@ Document.prototype.open = function(message) {
 
 
 Document.prototype.print = function() {
-  this.getDataUrl(function(dataUrl) {
-    var iFrame = document.createElement('iframe');
-    iFrame.style.position = 'absolute';
-    iFrame.style.left = '-99999px';
-    iFrame.src = dataUrl;
-    iFrame.onload = function() {
-      function removeIFrame(){
-        document.body.removeChild(iFrame);
-        document.removeEventListener('click', removeIFrame);
-      }
-      document.addEventListener('click', removeIFrame, false);
-    };
-
-    document.body.appendChild(iFrame);
-  }, { autoPrint: true });
+		// we have to open the window immediately and store the reference
+	// otherwise popup blockers will stop us
+	var win = window.open('', '_blank');
+	
+	try {
+		this.getBuffer(function (result) {
+			var blob;
+			try {
+				blob = new Blob([result], { type: 'application/pdf' });
+			} catch (e) {
+				// Old browser which can't handle it without making it an byte array (ie10) 
+				if (e.name == "InvalidStateError") {
+					var byteArray = new Uint8Array(result);
+					blob = new Blob([byteArray.buffer], { type: 'application/pdf' });
+				}
+			}
+			
+			if (blob) {
+				var urlCreator = window.URL || window.webkitURL;
+				var pdfUrl = urlCreator.createObjectURL( blob );
+				win.location.href = pdfUrl;
+			} else {
+				throw 'Could not generate blob';
+			}
+		},  { autoPrint: true });
+	} catch(e) {
+		win.close();
+		throw e;
+	}
 };
 
 Document.prototype.download = function(defaultFileName, cb) {
@@ -95,7 +127,7 @@ Document.prototype.download = function(defaultFileName, cb) {
            blob = new Blob([result], { type: 'application/pdf' });
        }
        catch (e) {
-           // Old browser which can't handle it without making it an byte array (ie10)
+           // Old browser which can't handle it without making it an byte array (ie10) 
            if (e.name == "InvalidStateError") {
                var byteArray = new Uint8Array(result);
                blob = new Blob([byteArray.buffer], { type: 'application/pdf' });
@@ -134,6 +166,8 @@ Document.prototype.getBuffer = function(cb, options) {
   });
 };
 
-module.exports = function(docDefinition, fonts, vfs) {
-	return new Document(docDefinition, fonts, vfs);
+module.exports = {
+	createPdf: function(docDefinition, fonts, vfs) {
+		return new Document(docDefinition, fonts, vfs);
+	}
 };
